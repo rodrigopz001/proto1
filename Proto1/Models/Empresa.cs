fc72d7cef326d2335483c1ffbca122d9ad369c63
Rodrigo.Pizarro.Zapata <rodrigo.pizarro@antaramining.cl>
2019-08-16 12:20:46 -0300

    Primer commit de la rama dev2.

---
+++ Proto1/Models/Empresa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Proto1.Models
+{
+    public class Empresa
+    {
+
+        public int id { get; set; }
+
+        public String nombre { get; set; }
+
+        public String ruta_bd { get; set; }
+
+    }
+}
